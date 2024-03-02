9f7f8c9dea8d1e15523358b43da6a8c48da89e07
Mateusz Wnuk <mateusz2003w@gmail.com>
2024-03-02 21:42:19 +0100

    initial commit

---
+++ src/main/java/com/example/carrentingapp/CarRentingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrentingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carrentingapp/CarRentingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carrentingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
